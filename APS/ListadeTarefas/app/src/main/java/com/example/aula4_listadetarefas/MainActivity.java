301b316af6d0f15361ccd59d77946096c5b4439d
Anne Livia <annelivia16@gmail.com>
2019-11-08 23:56:32 -0300

    assignment aps

--- APS/ListadeTarefas/app/src/main/java/com/example/aula4_listadetarefas/MainActivity.java
+++ APS/ListadeTarefas/app/src/main/java/com/example/aula4_listadetarefas/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.aula4_listadetarefas;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -26,7 +29,7 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText MeuTexto;
+    private EditText MeuTexto, MinhaData, MinhaHora;
     private ListView MinhaLista;
     private Button MeuBotao;
 
@@ -35,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayAdapter<String> ItensAdaptador;
     private ArrayList<Integer> ids;
-    private ArrayList<String> itens;
+    private ArrayList<String> itens, datahora, checked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
         MeuTexto = (EditText) findViewById(R.id.MeuTexto);
         MinhaLista = (ListView) findViewById(R.id.MinhaLista);
         MeuBotao = (Button) findViewById(R.id.MeuBotao);
+        MinhaData = (EditText) findViewById(R.id.MinhaData);
+        MinhaHora = (EditText) findViewById(R.id.MinhaHora);
 
         // carregar informações do banco de dados assim que carregar o app
         CarregaTarefas();
@@ -54,7 +59,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // passar para função adicionatarefa o texto digitado pelo usuario
-                AdicionaTarefas(MeuTexto.getText().toString());
+                // checar se data esta correta
+                if(isDate(MinhaData.getText().toString())) {
+                    if(isHour(MinhaHora.getText().toString())) {
+                        AdicionaTarefas(MeuTexto.getText().toString(), MinhaData.getText().toString(), MinhaHora.getText().toString());
+                    } else {
+                        Toast.makeText(MainActivity.this, "Hora não inserida ou no formato errado.", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Data não inserida ou no formato errado.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -75,52 +89,89 @@ public class MainActivity extends AppCompatActivity {
         MinhaLista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long l) {
-                try {
-                    TextView tt = (TextView) view;
-                    // Log.d("BancodeDados", "String: "+ tt.getText());
-
-                    boolean isCross = false;
-                    if (!tt.getPaint().isStrikeThruText()) {
-                        tt.setPaintFlags(tt.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-                        Toast.makeText(MainActivity.this, "Tarefa Concluída!", Toast.LENGTH_SHORT).show();
-                    } else {
-                        isCross = true;
-                        tt.setPaintFlags(tt.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
-                        Toast.makeText(MainActivity.this, "Tarefa Não Concluída!", Toast.LENGTH_SHORT).show();
-                    }
-
-                    // updateTarefa(posicao, isCross);
-
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
+                updateTarefa(posicao);
             }
         });
     }
 
-    /*
-    private void updateTarefa(int pos, boolean iscross) {
-        try{
-            Log.d("BancodeDados", "Strinsssg nao incluida: ");
-            // executar comando de delete onde o id for igual
-            BancoDados.execSQL("UPDATE MinhasTarefas SET checked=" + iscross +" WHERE id=" + pos);
-            // informar atualizacao
-            if(iscross) {
-            } else {
+
+    private boolean isDate(String date) {
+        // dd/mm/yyyy
+        if(date.length() == 10) {
+            if(date.charAt(2) == '/' && date.charAt(5) == '/') {
+                int n = 0;
+                for(int i = 0; i < 10; ++i) {
+                    int a = (date.charAt(i) - '0');
+                    if(i != 2 && i != 5 && a >= 0 && a <= 9) {
+                        n++;
+                    }
+                }
+
+                if(n == 8) {
+                    return true;
+                }
             }
+        }
+        return false;
+    }
+
+
+    private boolean isHour(String hour) {
+        Log.d("Banco de Dados", "N: " + hour);
+        // hh:mm
+        if(hour.length() == 5) {
+            if(hour.charAt(2) == ':') {
+                int n = 0;
+                for(int i = 0; i < 5; ++i) {
+                    int a = (hour.charAt(i) - '0');
+                    if(i != 2 && a >= 0 && a <= 9) {
+                        n++;
+                    }
+                }
+
+                if(n == 4) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void updateTarefa(int pos) {
+        try{
+            Integer posId = ids.get(pos);
+            // new value
+            int check = 1;
+            if(checked.get(pos).equals("1"))
+                check = 0;
+
+            Log.d("BancodeDados", "Atualizacao em " + posId + " para " + check);
+
+            // atualizando valor da coluna checked
+            ContentValues checkedNew = new ContentValues();
+            checkedNew.put("checked", Integer.toString(check));
+            // executar comando de incluir onde o id for igual pos
+            BancoDados.update("MinhasTarefas", checkedNew, "id = " + posId, null);
+            // informar atualizacao
+            if(check == 1) {
+                Toast.makeText(MainActivity.this, "Tarefa Concluída!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Tarefa Não Concluída!", Toast.LENGTH_SHORT).show();
+            }
+
             // atualizar list
             CarregaTarefas();
         } catch (Exception e){
             e.printStackTrace();
         }
     }
-    */
+
     private void CarregaTarefas(){
         try{
             // cria bd lista tarefa se não existir, ou abri se existe
             BancoDados = openOrCreateDatabase("ListaTarefas", MODE_PRIVATE, null);
             // cria tabela minhastarefa se não existir com id e tarefa como atributos
-            BancoDados.execSQL("CREATE TABLE IF NOT EXISTS MinhasTarefas(id INTEGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR, datahora VARCHAR)");
+            BancoDados.execSQL("CREATE TABLE IF NOT EXISTS MinhasTarefas(id INTEGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR, datahora VARCHAR, checked VARCHAR)");
 
             //String NovaTarefa = MeuTexto.getText().toString();
             //BancoDados.execSQL("INSERT INTO MinhasTarefas(tarefa) VALUES('" + NovaTarefa + "')");
@@ -132,10 +183,14 @@ public class MainActivity extends AppCompatActivity {
             // obtem id e tarefa
             int IndiceColunaID = cursor.getColumnIndex("id");
             int IndiceColunaTarefa = cursor.getColumnIndex("tarefa");
+            int IndiceColunaChecked = cursor.getColumnIndex("checked");
+            int IndiceColunaDataHora = cursor.getColumnIndex("datahora");
 
             // inicializando arrays globais  ids e itens
             itens = new ArrayList<String>();
             ids = new ArrayList<Integer>();
+            checked = new ArrayList<String>();
+            datahora = new ArrayList<String>();
 
             // itensadaptor é um objeto que
             /*
@@ -160,9 +215,32 @@ public class MainActivity extends AppCompatActivity {
 
                 */
             ItensAdaptador = new ArrayAdapter<String>(getApplicationContext(),
-                    android.R.layout.simple_list_item_1,
+                    android.R.layout.simple_list_item_2,
                     android.R.id.text1,
-                    itens);
+                    itens) {
+                @Override
+                // Sobrescreveno o getView para poder por o texto três
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    View view = super.getView(position, convertView, parent);
+                    TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                    TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+                    text1.setText(itens.get(position).toString());
+                    text2.setText(datahora.get(position).toString());
+
+                    text1.setTextColor(Color.BLACK);
+                    text2.setTextColor(Color.BLUE);
+
+                    // verificando no bd se o valor em cheched é 1 ou 0, se 1 riscar caso contrario tirar o risco
+                    if(checked.get(position).equals("1")) {
+                        text1.setPaintFlags(text1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                        text2.setPaintFlags(text1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                    } else {
+                        text1.setPaintFlags(text1.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+                        text2.setPaintFlags(text1.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+                    }
+                    return view;
+                }
+            };
 
             MinhaLista.setAdapter(ItensAdaptador);
 
@@ -170,9 +248,11 @@ public class MainActivity extends AppCompatActivity {
 
             // pegando todos os dados cadastrados no bd e populando os arrays de id e de itens
             while (cursor!=null){
-                Log.d("BancoDados", "ID: " + cursor.getString(IndiceColunaID) + " Tarefa: " + cursor.getString(IndiceColunaTarefa));
+                Log.d("BancoDados", "ID: " + cursor.getString(IndiceColunaID) + " Tarefa: " + cursor.getString(IndiceColunaTarefa) + "DataHora: " + cursor.getString(IndiceColunaDataHora));
                 itens.add(cursor.getString(IndiceColunaTarefa));
                 ids.add(Integer.parseInt(cursor.getString(IndiceColunaID)));
+                checked.add(cursor.getString(IndiceColunaChecked));
+                datahora.add(cursor.getString(IndiceColunaDataHora));
                 cursor.moveToNext();
             }
         } catch(Exception e){
@@ -180,7 +260,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void AdicionaTarefas(String tarefa){
+    private void AdicionaTarefas(String tarefa, String data, String hora){
         // Andorid Toast can be used to display information for the short period of time.
         // A toast contains message to be displayed quickly and disappears after sometime.
         if (tarefa.isEmpty()){
@@ -217,15 +297,15 @@ public class MainActivity extends AppCompatActivity {
             if(!tarefa.equals("")) {
                 // exibir na tela tarefa add
                 Toast.makeText(MainActivity.this, "Tarefa " + tarefa + " adicionada!", Toast.LENGTH_SHORT).show();
-                Date datahora = new Date();
-                String dia = new SimpleDateFormat("dd/MM/yyyy").format(datahora);
-                String hora = new SimpleDateFormat("HH:mm:ss").format(datahora);
-                String tempo = dia + " - " + hora;
-                tarefa+=" (" + tempo + ")";
+                String dia = data;
+                String horario = hora;
+                String tempo = dia + " - " + horario;
                 // esvaziar editlist
                 MeuTexto.setText("");
+                MinhaHora.setText("");
+                MinhaData.setText("");
                 // inserindo no bd
-                BancoDados.execSQL("INSERT INTO MinhasTarefas(tarefa, datahora) VALUES('" + tarefa + "', '" + tempo + "')");
+                BancoDados.execSQL( "INSERT INTO MinhasTarefas(tarefa, datahora, checked) VALUES('" + tarefa + "', '" + tempo + "', '" + 0 + "')");
                 // atualizar informações
                 CarregaTarefas();
             } else {
@@ -265,6 +345,5 @@ public class MainActivity extends AppCompatActivity {
                         ApagaTarefas(ids.get(NumeroId));
                     }
                 }).setNegativeButton("Não", null).show();
-
     }
 }
