df9facd81162db5085f178ff504cae43bb8b14f3
Anne Livia <annelivia16@gmail.com>
2020-11-17 16:28:09 -0300

    accepted

---
+++ TOPES/TOPESLojaNodeJs/routes/enderecos.js
@@ -0,0 +1,52 @@
+const rp = require('request-promise')
+const req = require('request')
+const bodyParser = require('body-parser')
+module.exports = app => {
+	const Enderecos = app.models.enderecos;
+	
+	app.get("/enderecos", (req, res) => {
+			Enderecos.findAll({},(retorno)=>
+							{res.json({enderecos: retorno})});
+	});
+
+  app.get('/enderecos/cep/:cep', (req, res) => {
+		var cep = req.params.cep;	
+		const getViaCep = {
+            uri: 'https://viacep.com.br/ws/'+cep+'/json/',
+            method: 'GET'            
+        }
+        
+		rp(getViaCep).then(function (parsedBody) {
+                res.send(parsedBody);
+            }).catch(function (err) { 
+                res.send("ERRO");
+            });
+	});
+
+// create application/json parser
+  var jsonParser = bodyParser.json()
+	app.post("/enderecos", jsonParser, function(req, res) {
+		
+ 			var idPessoa = req.body.idPessoa;
+      var logradouro = req.body.logradouro;
+      var numero = req.body.numero;
+      var complemento = req.body.complemento;
+      var bairro = req.body.bairro;
+      var cidade = req.body.cidade;
+      var estado = req.body.estado;
+      var tipoDeEndereco = req.body.tipoDeEndereco;
+
+			res.json(
+				{
+					idPessoaRecebido: idPessoa,
+					logradouroRecebido: logradouro,
+          numeroRecebido: numero,
+          complementoRecebido: complemento,
+          bairroRecebido: bairro,
+          cidadeRecebido: cidade,
+          estadoRecebido: estado,
+          tipoDeEnderecoRecebido: tipoDeEndereco,
+				}
+			)
+	});
+};
